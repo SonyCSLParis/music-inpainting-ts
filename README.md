717fdfb7b4fd77d2adb1143e44f605c3c2e8d780
Sebastian Haas <sebastian@haas.tech>
2017-04-10 19:14:18 +0200

    feat: updated example

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # webpack-usage-example
-An example how to use opensheetmusicdisplay with a webpack build.
+An example how to use OpenSheetMusicDisplay within a Webpack build. Uses TypeScript.
 
 ## Usage
 ```
 $ npm install
 $ npm start
 ```
-Now you can browse to http://127.0.0.1:8080 and see your running instance of opensheetmusicdisplay.
 
-If you make changes, you can trigger a rebuild using
+Now you can browse to http://127.0.0.1:8080 and see your running instance of
+OpenSheetMusicDisplay.
+
+If you decided to play around and make changes, you can trigger a rebuild anytime using
 ```
 $ npm run webpack
 ```
+
+## Project structure
+* `index.ts` - the application's entry point, contains all source
+* `webpack.config.js` - Webpack configuration
+* `tsconfig.json` - TypeScript compiler configuration
+* `MuzioClementi_SonatinaOpus36No1_Part1.xml` - the MusicXML file to be displayed
+
+### Build artifacts
+* `dist/` - directory containing all build artifacts, will be served on `npm start`
--- index.ts
+++ index.ts
@@ -2,14 +2,30 @@ import { OSMD } from "opensheetmusicdisplay";
 
 let osmd: OSMD;
 
-window.onload = () => {
-	let container: HTMLElement = <HTMLElement>document.createElement("div");
-    document.body.appendChild(container);
+/*
+ * Create a container element for OpenSheetMusicDisplay...
+ */
+let container: HTMLElement = <HTMLElement>document.createElement("div");
+/*
+ * ... and attach it to our HTML document's body. The document itself is a HTML5
+ * stub created by Webpack, so you won't find any actual .html sources.
+ */
+document.body.appendChild(container);
+/*
+ * Create a new instance of OpenSheetMusicDisplay and tell it to draw inside
+ * the container we've created in the steps before. The second parameter tells OSMD
+ * not to redraw on resize.
+ */
+osmd = new OSMD(container, false);
+/*
+ * Load our MusicXMl and display it. The file is renamed by Webpack during bundling, it's
+ * Muzio Clementi's Sonatina Opus 36 No 1, Part 1, which you can find next to this file.
+ */
+loadMusicXML("musicXmlSample.xml");
 
-    osmd = new OSMD(container, false);
-    loadMusicXML("musicXmlSample.xml");
-};
-
+/**
+ * Load a MusicXml file via xhttp request, and display its contents.
+ */
 function loadMusicXML(url: string) {
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function () {
@@ -28,7 +44,7 @@ function loadMusicXML(url: string) {
 						);
 	      	break;
 	      default:
-	      	throw("Error loading MusicXML.");
+	      	throw("Error loading MusicXML file.");
 	   }
 	}
    xhttp.open("GET", url, true);
