9aea3d4a7e7c4e7b2399b25e954f9f1096d0f9a5
Theis Bazin <theis.bazin@outlook.com>
2018-06-01 18:15:43 +0200

    Added fermata boxes

--- locator.ts
+++ locator.ts
@@ -50,24 +50,31 @@ export class eOSMD extends OpenSheetMusicDisplay {
     */
     private createTimeDiv(x, y, width, height, divClass: string, divId: string,
         onclick: (PointerEvent) => void): HTMLElement {
+        // container for positioning the timestamp box and attached boxes
+        const commonDivId = divId + '-common'
+        let commonDiv = (document.getElementById(commonDivId) ||
+            document.createElement("div"))
         let div = (document.getElementById(divId) ||
             document.createElement("div"))
 
-        div.style.top = this.computePositionZoom(y, 0) + 'px';
-        div.style.height = this.computePositionZoom(height, 0) + 'px';
-        div.style.left = this.computePositionZoom(x, 1) + 'px';
-        div.style.width = this.computePositionZoom(width) + 'px';
+        commonDiv.style.top = this.computePositionZoom(y, 0) + 'px';
+        commonDiv.style.height = this.computePositionZoom(height, 0) + 'px';
+        commonDiv.style.left = this.computePositionZoom(x, 1) + 'px';
+        commonDiv.style.width = this.computePositionZoom(width) + 'px';
 
-        if (div.id !== divId) {
+        console.log(commonDiv)
+        if (commonDiv.id !== commonDivId) {
             // the div was created in this call, it has no ID set yet
-            div.id = divId;
+            commonDiv.id = commonDivId;
+            commonDiv.classList.add('timecontainer')
+            div.id = divId
             div.classList.add('notebox');
             div.classList.add('available');
             div.classList.add(divClass);
             let granularitySelect : HTMLSelectElement = <HTMLSelectElement>document.getElementById('select-granularity')
-            if (granularitySelect.value == divClass){
-                div.classList.add('active')
-            }
+            const currentGranularity = granularitySelect[
+                parseInt(granularitySelect.value)].textContent
+            if (currentGranularity == divClass) div.classList.add('active')
 
             // // insert NipppleJS manager
             // var options = {
@@ -77,16 +84,45 @@ export class eOSMD extends OpenSheetMusicDisplay {
             // var manager = nipplejs.create(options);
             // var joystick_data = {};
             // var last_click = [];
-
             div.addEventListener('click', onclick);
+            // use bubbling and preventDefault to block window scrolling
+            div.addEventListener('wheel', function(event){
+                event.preventDefault();
+                let scrollUp = (-event.deltaY>=0)
+                cycleGranularity(scrollUp)
+            }, false);
 
             // add div to the rendering backend's <HTMLElement> for positioning
             let inner = this.renderingBackend.getInnerElement();
-            inner.appendChild(div);
+            inner.appendChild(commonDiv);
+            commonDiv.appendChild(div);
+
+            if (divClass.search('quarter') >= 0) {
+                // add fermata selection box
+                this.drawFermataBox(div);
+            }
         }
 
         return div;
     };
+
+    private drawFermataBox(timestampDiv: HTMLElement) {
+        // Position fermata box over `timestampElement`
+        // FIXME hardcoded quarter-note duration
+        const fermataDivId = timestampDiv.id + '-fermata'
+        let fermataDiv = (document.getElementById(fermataDivId) ||
+            document.createElement("div"));
+        fermataDiv.setAttribute('containedquarternotes',
+            timestampDiv.getAttribute('containedquarternotes'));
+        fermataDiv.classList.add('fermata');
+
+        fermataDiv.addEventListener('click', () => {
+            // $('.fermata').removeClass('active')
+            fermataDiv.classList.toggle('active')
+        })
+
+        timestampDiv.parentNode.appendChild(fermataDiv);
+    }
 
     public drawTimestampBoxes(onclickFactory=undefined): void{
         // FIXME this assumes a time signature of 4/4
@@ -109,6 +145,8 @@ export class eOSMD extends OpenSheetMusicDisplay {
 
         for (let measureIndex in measureList){
             let measure: StaffMeasure = <StaffMeasure>measureList[measureIndex][0]
+            let beginInstructionsWidth: number = measure.beginInstructionsWidth
+
             let sourceMeasure: SourceMeasure = measure.parentSourceMeasure;
             let absoluteTimestamp: Fraction = sourceMeasure.AbsoluteTimestamp;
             let musicSystem = measure.parentMusicSystem;
@@ -145,10 +183,21 @@ export class eOSMD extends OpenSheetMusicDisplay {
                                 measure.PositionAndShape.Size.width) + 1
                         }
 
+                        if (beginInstructionsWidth>1) {
+                            if (beginInstructionsWidth > 5) {
+                                xLeft -= 1  // HACK hardcoded
+                                xRight -= 1
+                            }
+                            else {
+                                xLeft -= 2
+                                xRight -= 2
+                            }
+                        }
                         let width = xRight - xLeft;
                         let onclick = (event) => {};
                         if (onclickFactory) {onclick = onclickFactory(leftTimestamp, rightTimestamp)}
-                        let timediv = this.createTimeDiv(xLeft, y, width, height,
+                        let timediv = this.createTimeDiv(
+                            xLeft, y, width, height,
                             granularityName,
                             `${granularityName}-${measureIndex}-${timestampIndex}`,
                             onclick)
@@ -174,3 +223,20 @@ export class eOSMD extends OpenSheetMusicDisplay {
     }
 
 }
+
+function cycleGranularity(increase: boolean) {
+    let granularitySelect = $('#select-granularity')
+    if (granularitySelect.length > 0) {
+        let granularitySelectElem = <HTMLSelectElement>granularitySelect[0]
+        const selectedGranularity = parseInt(granularitySelect.val().toString());
+        const numOptions = granularitySelect.children.length
+        if (increase) {
+            granularitySelectElem.value =
+                Math.min(selectedGranularity + 1, numOptions).toString()
+        }
+        else {
+            granularitySelectElem.value =
+                Math.max(selectedGranularity - 1, 0).toString()}
+        // trigger `onchange` callback
+        granularitySelectElem.dispatchEvent(new Event('change'))
+}}
