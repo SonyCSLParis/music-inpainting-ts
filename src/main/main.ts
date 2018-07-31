47d80d3cd8af03bcb9fba1455dd93c62cb8db774
Theis Bazin <theis.bazin@outlook.com>
2018-07-31 12:12:18 +0200

    :bug: Fix import of main HTML

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 import {app, BrowserWindow, ipcMain} from 'electron'
 import * as log from 'loglevel'
+log.enableAll()
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -11,10 +12,10 @@ function createWindow() {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadFile('./index.html')
+  mainWindow.loadFile(__dirname + '/index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
