b8d0ebc72c8029cacd6accca5ebd22bff1be822c
Theis Bazin <theis.bazin@outlook.com>
2018-08-08 17:52:15 +0200

    :rocket: Clean-up webpack configurations

--- webpack.main.js
+++ webpack.main.js
@@ -6,33 +6,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    // target: "electron-main",
-	// context: __dirname + "/src/main",
-    // entry: './main.ts',
-    // output: {
-    //   path: __dirname + '/app',
-    //   filename: 'main.js'
-    // },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-            filename: __dirname + '/app/index.html',
-            title: 'DeepBach',
-            favicon: '../common/images/favicon.ico',
-            inject: false
-        }),
-
-        new CopyWebpackPlugin([
-              {from: '../common/tonejs-instruments/samples/organ',
-               to: 'tonejs-instruments/samples/organ'},
-              {from: '../common/tonejs-instruments/samples/harmonium',
-               to: 'tonejs-instruments/samples/harmonium'},
-              {from: '../common/tonejs-instruments/samples/xylophone',
-               to: 'tonejs-instruments/samples/xylophone'}
-        ])
-    ],
-
     externals: {
         // stops webpack from trying to bundle ableton-link, which is a external
         // native dependency
