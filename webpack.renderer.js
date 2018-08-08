bb8721c104711dac04db51330ad42717be89c205
Theis Bazin <theis.bazin@outlook.com>
2018-08-08 17:52:15 +0200

    :rocket: Clean-up webpack configurations

--- webpack.renderer.js
+++ webpack.renderer.js
@@ -2,12 +2,4 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    // target: "electron-renderer",
-	// context: __dirname + "/src/renderer",
-    // entry: './index.ts',
-    // output: {
-    //   path: __dirname + '/app',
-    //   filename: 'renderer.js'
-    // },
-
 });
