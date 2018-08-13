f65e314ffb4c1bd1cc2f1329e1fddadddbd66dcc
Theis Bazin <theis.bazin@outlook.com>
2018-08-13 12:25:05 +0200

    :package: Separate window management and main back-end process

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,62 +1,19 @@
-// Modules to control application life and create native browser window
-import {app, BrowserWindow, ipcMain} from 'electron'
-import { format as formatUrl } from 'url'
-import * as path from 'path';
+// Modules to control application life
+import {app, ipcMain} from 'electron'
 import * as log from 'loglevel'
-const abletonlink = require('abletonlink');
+
+import * as WindowManager from './windowManager'
+import * as LinkServer from './linkServer'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-if (isDevelopment) {
-    log.setLevel('debug')
-}
-else {
-    log.setLevel('info')
-}
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow: BrowserWindow;
-
-function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow()
-
-  if (isDevelopment) {
-    mainWindow.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools()
-  }
-  else {
-    mainWindow.loadURL(formatUrl({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file',
-      slashes: true
-    }))
-  }
-  // TODO(theis) Remove this!
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-
-  mainWindow.webContents.on('devtools-opened', () => {
-      mainWindow.focus()
-      setImmediate(() => {
-          mainWindow.focus()
-      })
-  })
-}
+if (isDevelopment) { log.setLevel('debug'); }
+else { log.setLevel('info'); }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', WindowManager.createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -70,8 +27,8 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
+  if (!WindowManager.existsWindow()) {
+    WindowManager.createWindow()
   }
 })
 
---
+++ src/main/windowManager.ts
@@ -0,0 +1,55 @@
+// Module to create native browser window
+import {BrowserWindow} from 'electron'
+import { format as formatUrl } from 'url'
+import * as path from 'path';
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow: BrowserWindow;
+
+export function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow()
+
+  if (isDevelopment) {
+    mainWindow.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+  }
+  else {
+    mainWindow.loadURL(formatUrl({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file',
+      slashes: true
+    }))
+  }
+  // TODO(theis) Remove this!
+  mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+
+  mainWindow.webContents.on('devtools-opened', () => {
+      mainWindow.focus()
+      setImmediate(() => {
+          mainWindow.focus()
+      })
+  })
+}
+
+
+export function existsWindow(): boolean {
+    return mainWindow !== null
+}
+
+// send a message to the main Renderer window over IPC
+export function send(channel: string, ...args: any[]) {
+    mainWindow.webContents.send(channel, ...args)
+}
