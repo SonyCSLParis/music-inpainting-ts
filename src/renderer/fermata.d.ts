afee28fd230b54eb69ffdcc3dcebce06177e9ec6
Theis Bazin <theis.bazin@outlook.com>
2018-08-14 18:26:33 +0200

    :fire: Remove build-generated type-signatures

--- src/renderer/fermata.d.ts
+++
@@ -1,9 +0,0 @@
-import '../common/styles/overlays.scss';
-import { AnnotationBox } from './annotationBox';
-export declare class FermataBox extends AnnotationBox {
-    constructor(timestampContainer: string | HTMLElement, sequenceDuration: number);
-    protected validateTimestampContainer(): void;
-    private sequenceDuration;
-    private containedQuarterNote;
-    draw(): void;
-}
