d353227bcc5a3a0e189e0b0082a4cc1040ec0220
Theis Bazin <theis.bazin@outlook.com>
2018-09-13 18:11:35 +0200

    :fire: Remove code for NexusUI-based textual play button

--- src/renderer/playbackCommands.ts
+++ src/renderer/playbackCommands.ts
@@ -5,14 +5,11 @@ import '@fortawesome/fontawesome-free/css/all.css'
 // library.add(icon({ prefix: 'fas', iconName: 'stop-circle' }));
 // library.add(icon({ prefix: 'fas', iconName: 'play-circle' }));
 
-let Nexus = require('./nexusColored');
-
 import * as Playback from './playback'
 
 let pressPlay;
 let pressStop;
-
-export function render(useIcon: boolean=true): void{
+export function render(): void{
     let topControlsGridElem = document.getElementById('bottom-controls')
     let playbuttonElem: HTMLElement = document.createElement('control-item');
     playbuttonElem.id = 'play-button'
@@ -31,57 +28,49 @@ export function render(useIcon: boolean=true): void{
         })
     }
 
-    if (useIcon) {
-        let stoppedClass: string = 'fa-play-circle';
-        let playingClass: string = 'fa-stop-circle';
-        let waitingClass: string = 'fa-circle-notch';
+    let stoppedClass: string = 'fa-play-circle';
+    let playingClass: string = 'fa-stop-circle';
+    let waitingClass: string = 'fa-circle-notch';
 
-        let mainIconSize: string = 'fa-4x';
+    let mainIconSize: string = 'fa-4x';
 
-        let playButton = document.createElement('i');
-        playbuttonElem.appendChild(playButton)
-        playButton.classList.add('fas');
-        playButton.classList.add(mainIconSize);
-        playButton.style.cursor = 'pointer';
-        // FIXME not super visible
-        playButton.style.color = 'lightpink';
+    let playButton = document.createElement('i');
+    playbuttonElem.appendChild(playButton)
+    playButton.classList.add('fas');
+    playButton.classList.add(mainIconSize);
+    playButton.style.alignSelf = 'inherit';
+    playButton.style.cursor = 'pointer';
+    // FIXME not super visible
+    playButton.style.color = 'lightpink';
 
-        function setPlayingClass(isPlaying: boolean) {
-            // Update Play/Stop CSS classes
-            unsetWaitingClass();
-            if (isPlaying) {
-                playButton.classList.add(playingClass);
-                playButton.classList.remove(stoppedClass);
-            }
-            else {
-                playButton.classList.add(stoppedClass);
-                playButton.classList.remove(playingClass);
-            }
-        }
-        function setWaitingClass() {
-            // Replace the playback icon with a rotating 'wait' icon until
-            // playback state correctly updated
-            playButton.classList.remove(playingClass);
+    function setPlayingClass(isPlaying: boolean) {
+        // Update Play/Stop CSS classes
+        unsetWaitingClass();
+        if (isPlaying) {
+            playButton.classList.add(playingClass);
             playButton.classList.remove(stoppedClass);
+        }
+        else {
+            playButton.classList.add(stoppedClass);
+            playButton.classList.remove(playingClass);
+        }
+    }
+    function setWaitingClass() {
+        // Replace the playback icon with a rotating 'wait' icon until
+        // playback state correctly updated
+        playButton.classList.remove(playingClass);
+        playButton.classList.remove(stoppedClass);
 
-            playButton.classList.add('fa-spin');  // spinning icon
-            playButton.classList.add(waitingClass);
-        }
-        function unsetWaitingClass() {
-            // Remove rotating 'wait' icon
-            playButton.classList.remove('fa-spin');  // spinning icon
-            playButton.classList.remove(waitingClass);
-        }
-        // Initialize playback to stopped
-        setPlayingClass(false);
-
-        function playCallback(play: boolean) {
-            setWaitingClass();
-            playbackCallback(play).then(() => {
-                unsetWaitingClass();
-                setPlayingClass(play);
-            })
-        }
+        playButton.classList.add('fa-spin');  // spinning icon
+        playButton.classList.add(waitingClass);
+    }
+    function unsetWaitingClass() {
+        // Remove rotating 'wait' icon
+        playButton.classList.remove('fa-spin');  // spinning icon
+        playButton.classList.remove(waitingClass);
+    }
+    // Initialize playback to stopped
+    setPlayingClass(false);
 
         pressPlay = () => {playCallback(true);}
 
@@ -91,14 +80,6 @@ export function render(useIcon: boolean=true): void{
             playCallback(playButton.classList.contains(stoppedClass));
         });
     }
-    else {
-        // use a textual button
-        let playButton = new Nexus.TextButton('#play-button',{
-            'size': [150,50],
-            'state': false,
-            'text': 'Play',
-            'alternateText': 'Stop'
-        })
 
         playButton.on('change', (state) => playbackCallback(state))
 
