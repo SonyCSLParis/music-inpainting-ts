6829f73a05e58d9d8c50865fa84de056801ee914
Theis Bazin <theis.bazin@outlook.com>
2018-09-14 12:31:22 +0200

    :package: Provide static assets for web-target compilation

--- webpack.web.js
+++ webpack.web.js
@@ -90,7 +90,9 @@ module.exports = function (options) {
           {from: path.join(__dirname, 'src/renderer/tonejs-instruments/samples/organ'),
            to: 'tonejs-instruments/samples/organ'},
            {from: path.join(__dirname, 'src/renderer/tonejs-instruments/samples/xylophone'),
-           to: 'tonejs-instruments/samples/xylophone'}
+           to: 'tonejs-instruments/samples/xylophone'},
+          {from: path.join(__dirname, 'static'),
+           to: 'static'}
       ])
     ],
 
