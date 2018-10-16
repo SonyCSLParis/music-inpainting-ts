bf878b47c1174e6d2a0ea612606cf9ac888bf8d4
Theis Bazin <theis.bazin@outlook.com>
2018-10-16 17:04:23 +0200

    :sparkles: Loop help-tour when application is idle

--- src/renderer/helpTour.ts
+++ src/renderer/helpTour.ts
@@ -1,4 +1,5 @@
 import * as $ from "jquery";
+import createActivityDetector from 'activity-detector';
 
 import "trip.js/dist/trip.css";
 
@@ -11,13 +12,13 @@ let noteboxHelp_english: string = "Touch the <b>blue boxes</b> to trigger a \
 
 let fermataboxHelp_german: string = 'Tippen Sie auf die <b>gestrichelten Kästchen</b>, um \
 eine <b>Fermata</b> zu setzen. Damit kann die Struktur des Chorales kontrolliert werden';
-let fermataboxHelp_english: string = 'Touch the <b>dotted boxes</b> to position an intermediate <b>fermata</b>. It helps to structure the chorale'
+let fermataboxHelp_english: string = 'Touch the <b>dotted boxes</b> to position an intermediate <b>fermata</b>. It allows to structure the chorale'
 
 function makeHTMLContent(german: string, english: string) {
     return `${german}<br><br><i>${english}</i><br><br>`
 }
 
-let trip = new Trip([
+const tripContents: object[] = [
         // div[id$='-note-0-0']
         { sel: "#whole-note-0-0-common",
           content: makeHTMLContent(noteboxHelp_german, noteboxHelp_english),
@@ -31,17 +32,23 @@ let trip = new Trip([
         },
         // { sel: '#app-title', content: "Press play to listen to the generated music!",
         //     position: 'n'}
-    ],
+    ]
+
+const tripDelay_ms: number = 10000;
+
+const totalTripDuration_ms: number = tripDelay_ms * tripContents.length;
+
+const trip = new Trip(tripContents,
     {
         showSteps: true,
-        onTripStart: initHideOnClickOutside,
-        onTripEnd: removeClickListener,
+        onStart: initHideOnClickOutside,
+        onEnd: removeClickListener,
         onTripStop: removeClickListener,
         overlayZIndex: 0,
         nextLabel: '→',
         prevLabel: '←',
         skipLabel: '',
-        finishLabel: '',
+        finishLabel: 'x',
         tripTheme: 'white',
         showNavigation : true,
         // showCloseBox : true,
@@ -49,10 +56,34 @@ let trip = new Trip([
   }
 );
 
+let tripLoopInterval: any;
+
+function startTripLoop() {
+    // starts the help tour in a looping fashion
+
+    function intervalTripLoop() {
+        tripLoopInterval = setInterval(() => {
+            // if (looping) {
+                trip.start();
+            // }
+        },
+        totalTripDuration_ms + 500)
+    }
+    trip.start();
+    intervalTripLoop();
+}
+
+function stopTripLoop() {
+    // stops the help tour from looping
+    clearInterval(tripLoopInterval);
+}
+
+
 function outsideClickListener(event) {
-    let target: HTMLElement = event.target
+    let target: HTMLElement = event.target;
     if (!$(target).closest($('div.trip-block')).length) {
-        trip.stop()
+        stopTripLoop();
+        trip.stop();
     }
 };
 
@@ -64,7 +95,7 @@ function outsideClickListener(event) {
 
      // must add a delay so that the initial click is
      // setTimeout(() => {
-        document.addEventListener('click', outsideClickListener);
+    document.addEventListener('click', outsideClickListener);
      // },
      // 100);
  };
@@ -87,7 +118,23 @@ export function render(containerElement: HTMLElement) {
         trip.start()
     }, true);
 }
-// function tourManagerCallback(): void {
-// }
+
+export function registerIdleStateDetector(): void {
+    const activityDetector = createActivityDetector({
+        timeToIdle: 2 * 1000 * 60,  // launch after 2 minutes of inactivity
+        autoInit: true,
+        inactivityEvents: [],
+    });
+
+    activityDetector.on('idle', () => {
+    	console.log('The user is not interacting with the page');
+        startTripLoop();
+    });
+
+    activityDetector.on('active', () => {
+        console.log('The user is interacting with the page');
+        stopTripLoop();
+    })
+}
 
 if (module.hot) {}
