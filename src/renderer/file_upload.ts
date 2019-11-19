50c4289d91b9dcfeee3fbbb1b9d7f67467a48d27
Theis Bazin <theis.bazin@outlook.com>
2019-11-19 14:16:33 +0100

    :fire: Remove unused fine-upload dependency

--- src/renderer/file_upload.ts
+++ src/renderer/file_upload.ts
@@ -7,66 +7,43 @@ let serverConfig: object = require('../common/config.json')
 let serverUrl = `http://${serverConfig['server_ip']}:${serverConfig['chorale_port']}/`;
 let generateArgs: string = 'analyze-audio';
 
-let useFineUpload: boolean = false;
-
 export function createWavInput(onSuccess: Function): void {
-  if (useFineUpload) {
-      import('fine-uploader')
-      .then(qq => {
-          require('fine-uploader/fine-uploader/fine-uploader-gallery.css')
-          let gallery_template: string = require('fine-uploader/all.fine-uploader/templates/gallery.html')
-          let fineUploadGalleryTemplateElement: HTMLElement = document.createElement('div')
-          document.body.appendChild(fineUploadGalleryTemplateElement)
-          fineUploadGalleryTemplateElement.innerHTML = gallery_template
+    let simpleUpload: HTMLDivElement = document.createElement('div')
+    document.body.appendChild(simpleUpload)
+    let simpleUpload_html: string =
+        `<div id="filename"></div>
+        <div id="progress"></div>
+        <div id="progressBar"></div>
 
-          let uploaderDiv: HTMLDivElement = document.createElement('div');
-          uploaderDiv.id = 'uploader';
-          document.body.appendChild(uploaderDiv);
+        <label for="wav-input">Select a .wav file to upload</label>
+        <input type="file" name="audio" accept="audio/x-wav" id="wav-input"/>`
+    simpleUpload.innerHTML = simpleUpload_html;
 
-          var uploader = new qq.FineUploader({
-                      element: document.getElementById("uploader")
-                  });
-      }
-  )
-  }
-  else {
-      let simpleUpload: HTMLDivElement = document.createElement('div')
-      document.body.appendChild(simpleUpload)
-      let simpleUpload_html: string =
-          `<div id="filename"></div>
-          <div id="progress"></div>
-          <div id="progressBar"></div>
+    $('#wav-input').change(function() {
+        $(this).simpleUpload(serverUrl + generateArgs, {
+            start: function(file){
+                    //upload started
+                    $('#filename').html(file.name);
+                    $('#progress').html("");
+                    $('#progressBar').width(0);
+                },
 
-          <label for="wav-input">Select a .wav file to upload</label>
-          <input type="file" name="audio" accept="audio/x-wav" id="wav-input"/>`
-      simpleUpload.innerHTML = simpleUpload_html;
+                progress: function(progress){
+                    //received progress
+                    $('#progress').html("Progress: " + Math.round(progress) + "%");
+                    $('#progressBar').width(progress + "%");
+                },
 
-      $('#wav-input').change(function() {
-          $(this).simpleUpload(serverUrl + generateArgs, {
-              start: function(file){
-        				//upload started
-        				$('#filename').html(file.name);
-        				$('#progress').html("");
-        				$('#progressBar').width(0);
-  			       },
+                success: function(data){
+                    //upload successful
+                    $('#progress').html("Success!<br>Data: " + JSON.stringify(data));
+                onSuccess()
+                },
 
-          			progress: function(progress){
-          				//received progress
-          				$('#progress').html("Progress: " + Math.round(progress) + "%");
-          				$('#progressBar').width(progress + "%");
-          			},
-
-          			success: function(data){
-          				//upload successful
-          				$('#progress').html("Success!<br>Data: " + JSON.stringify(data));
-                  onSuccess()
-          			},
-
-          			error: function(error){
-          				//upload failed
-          				$('#progress').html("Failure!<br>" + error.name + ": " + error.message);
-          			}
-          })
-  	});
-  }
+                error: function(error){
+                    //upload failed
+                    $('#progress').html("Failure!<br>" + error.name + ": " + error.message);
+            }
+        })
+    });
 }
