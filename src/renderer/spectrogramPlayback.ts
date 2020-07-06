eb085a4c36db86db4168418de88546cba79fecf8
Theis Bazin <theis.bazin@outlook.com>
2020-07-06 12:13:26 +0200

    :zap: Crossfade for live spectrogram updates

--- src/renderer/spectrogramPlayback.ts
+++ src/renderer/spectrogramPlayback.ts
@@ -7,41 +7,77 @@ let Nexus = require('./nexusColored');
 
 export class SpectrogramPlaybackManager extends PlaybackManager {
     readonly spectrogramLocator: Spectrogram;
-    private player: Tone.Player = new Tone.Player().toDestination();
+    // initialize crossfade to play player A
+    private crossFade: Tone.CrossFade = new Tone.CrossFade(0).toDestination();
+    private player_A: Tone.Player = new Tone.Player().connect(this.crossFade.a);
+    private player_B: Tone.Player = new Tone.Player().connect(this.crossFade.b);
+
+    protected crossFadeDuration: Tone.Unit.Time = "0.5";
+    protected crossFadeOffset: Tone.Unit.Time = "+1";
+
+    public get duration_s(): number {
+        return this.currentPlayer().buffer.duration;
+    };
 
     protected setPlaybackPositionDisplay(timePosition: number): void {
         this.spectrogramLocator.setPosition(timePosition);
     }
 
     protected getCurrentDisplayTimestep(): number {
-        return 0;
+        // TODO(theis): fix method name, this returns the ratio of progress in the playback
+        return Tone.Transport.progress;
     }
 
-    private schedulePlaybackLoop() {
-        this.player.sync();
-        Tone.Transport.setLoopPoints(0, 4);
+    protected currentPlayer(): Tone.Player {
+        return this.crossFade.fade.getValueAtTime(this.crossFadeOffset) > 0.5 ? this.player_B : this.player_A
+    }
+
+    protected nextPlayer(): Tone.Player {
+        return this.crossFade.fade.getValueAtTime(this.crossFadeOffset) > 0.5 ? this.player_A : this.player_B
+    }
+
+    protected switchPlayers() {
+        const currentScheduledValue: number = this.crossFade.fade.getValueAtTime(this.crossFadeOffset);
+        console.log(currentScheduledValue);
+        const newValue: number = Math.round(1 - currentScheduledValue);  // round ensures binary values
+        this.crossFade.fade.exponentialRampTo(newValue, this.crossFadeDuration);
+    }
+
+    private scheduleInitialPlaybackLoop() {
+        this.player_A.sync();
+        this.player_B.sync();
         Tone.Transport.loop = true;
-        // stoping playback at every loop ensures that potential new audio
-        // gets reloaded as it comes
-        this.player.start(0).stop(3.99);
+    };
+
+    private reschedulePlaybackLoop() {
+        this.nextPlayer().unsync();
+        this.nextPlayer().stop();
+        this.nextPlayer().sync();
+        Tone.Transport.setLoopPoints(0, this.nextPlayer().buffer.duration);
+        this.nextPlayer().start(0);
     };
 
     async loadAudio(audioURL: string): Promise<void> {
-        await this.player.load(audioURL);
+        await this.nextPlayer().load(audioURL);
+        this.reschedulePlaybackLoop();
+        this.switchPlayers();
     };
 
     loadSpectrogram(serverURL: string, command: string, codes: number[][]): void {
-
+        // TODO(theis)
+        throw new Error("Not implemented");
     }
 
-    constructor(duration_s: number, spectrogramLocator: Spectrogram) {
+    constructor(spectrogramLocator: Spectrogram) {
         super();
-        this.schedulePlaybackLoop();
         this.spectrogramLocator = spectrogramLocator;
+
+        this.scheduleInitialPlaybackLoop();
     }
 
     setFadeIn(duration_s: number) {
-        this.player.fadeIn = duration_s
+        this.player_A.fadeIn = duration_s
+        this.player_B.fadeIn = duration_s
     }
 };
 
