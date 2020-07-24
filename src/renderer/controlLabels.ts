7c99ba5908894b94469227195d9bb66df3675feb
Theis Bazin <theis.bazin@outlook.com>
2020-07-24 18:58:04 +0200

    :bug: :recycle: Fix fonts weights management

--- src/renderer/controlLabels.ts
+++ src/renderer/controlLabels.ts
@@ -1,11 +1,11 @@
-let configuration = require('../common/default_config.json');
-let localizations = require('../common/localization.json');
+const configuration = require('../common/default_config.json');
+const localizations = require('../common/localization.json');
 
 export function createLabel(controlElem: HTMLElement, id: string) {
     let labelElem: HTMLElement = document.createElement('control-label');
     labelElem.id = id;
-    let controlLabel: string = `<b>${
-        localizations['control-labels'][id][configuration["main_language"]]}</b>`;
+    let controlLabel: string = `${
+        localizations['control-labels'][id][configuration["main_language"]]}`;
     const secondary_language = configuration["secondary_language"];
     if (secondary_language && !(secondary_language === "")) {
         controlLabel.concat(`<br><i>${localizations['control-labels'][id][secondary_language]}</i>`)
