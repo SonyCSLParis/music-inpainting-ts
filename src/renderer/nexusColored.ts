c9cfb46fdf9ec8e16ae0b27cffe24cac856415c8
Théis Bazin <theis.bazin@outlook.com>
2021-01-20 16:30:14 +0100

    :recycle: Remove use of ICSS in webpack-css-loader
    * Switch to standard module mode

--- src/renderer/nexusColored.ts
+++ src/renderer/nexusColored.ts
@@ -1,7 +1,7 @@
 // Simple module to store and share the color parameters for Nexus UI
 
 import * as Nexus from 'nexusui';
-import colors from '../common/styles/mixins/_colors.scss';
+import colors from '../common/styles/mixins/_colors.module.scss';
 
 Nexus.colors.accent = colors.active_control;
 Nexus.colors.fill = colors.idle_control;
