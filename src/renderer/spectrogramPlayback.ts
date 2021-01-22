c7771693f04156c95b0ce2e68cce0be5237b1418
Théis Bazin <theis.bazin@outlook.com>
2021-01-22 19:13:20 +0100

    :sparkles: Add MIDI-channel filtering support

--- src/renderer/spectrogramPlayback.ts
+++ src/renderer/spectrogramPlayback.ts
@@ -6,6 +6,7 @@ import { SpectrogramLocator } from "./locator";
 import Nexus from './nexusColored';
 
 import { getMidiInputListener } from './midiIn';
+import { IMidiChannel } from 'webmidi';
 
 interface NoteEvent {
 	note: string;
@@ -28,7 +29,7 @@ export class SpectrogramPlaybackManager extends PlaybackManager<SpectrogramLocat
 
     protected crossFadeDuration: Tone.Unit.Time = "1";
     // look-ahead duration to retrieve the state of the crossfade after potential fading operations
-    protected crossFadeOffset: Tone.Unit.Time = "+1.5";
+    protected crossFadeOffset: Tone.Unit.Time = "+1.1";
 
     constructor(locator: SpectrogramLocator) {
         super(locator);
@@ -36,16 +37,22 @@ export class SpectrogramPlaybackManager extends PlaybackManager<SpectrogramLocat
         this.scheduleInitialPlaybackLoop();
 
         getMidiInputListener().then((midiListener) => {
-            midiListener.on('keyDown', (data: NoteEvent) => {
-                this.currentSampler().triggerAttack(
-                    data.note, undefined, data.velocity);
-                return this
-            });
-            midiListener.on('keyUp', (data: NoteEvent) => {
-                this.currentSampler().triggerRelease(data.note);
-            });
+            if ( midiListener !== null ) {
+                midiListener.on('keyDown', this.onkeydown.bind(this))
+                midiListener.on('keyUp', this.onkeyup.bind(this));
+            }
         });
     }
+
+    protected onkeyup(data: NoteEvent) {
+        this.currentSampler().triggerRelease(data.note);
+        return this
+    }
+
+    protected onkeydown(data: NoteEvent) {
+        this.currentSampler().triggerAttack(data.note, undefined, data.velocity);
+        return this
+    };
 
     // duration of the currently playing player in seconds
     public get duration_s(): number {
@@ -65,7 +72,12 @@ export class SpectrogramPlaybackManager extends PlaybackManager<SpectrogramLocat
         return this.crossFade.fade.getValueAtTime(this.crossFadeOffset) <= 0.5;
     }
 
-    // return the player scheduled to play after any eventual crossfade operation has been completed
+    // return the buffer scheduled to play after any eventual crossfade operation has been completed
+    protected currentBuffer(): Tone.ToneAudioBuffer {
+        return this.currentPlayerIsA() ? this.buffer_A : this.buffer_B
+    }
+
+    // return the sampler scheduled to play after any eventual crossfade operation has been completed
     protected currentSampler(): Tone.Sampler {
         return this.currentPlayerIsA() ? this.sampler_A : this.sampler_B
     }
@@ -80,7 +92,7 @@ export class SpectrogramPlaybackManager extends PlaybackManager<SpectrogramLocat
         return this.currentPlayerIsA() ? this.player_B : this.player_A
     }
 
-    // return the player scheduled to be idle after any eventual crossfade operation has been completed
+    // return the buffer scheduled to be idle after any eventual crossfade operation has been completed
     protected nextBuffer(): Tone.ToneAudioBuffer {
         return this.currentPlayerIsA() ? this.buffer_B : this.buffer_A;
     }
@@ -93,7 +105,7 @@ export class SpectrogramPlaybackManager extends PlaybackManager<SpectrogramLocat
     }
 
     // initialize the Transport loop and synchronize the two players
-    private scheduleInitialPlaybackLoop() {
+    protected scheduleInitialPlaybackLoop() {
         this.player_A.sync();
         this.player_B.sync();
         Tone.getTransport().loop = true;
@@ -126,11 +138,7 @@ export class SpectrogramPlaybackManager extends PlaybackManager<SpectrogramLocat
     }
 
     setFadeIn(duration_s: number) {
-        this.player_A.fadeIn = duration_s
-        this.player_B.fadeIn = duration_s
-
-        this.sampler_A.attack = Math.min(duration_s, 1);
-        this.sampler_B.attack = Math.min(duration_s, 1);
+        this.player_A.fadeIn = this.player_B.fadeIn = this.sampler_A.attack = this.sampler_B.attack = duration_s;
     }
 
     get Gain(): number {
@@ -148,12 +156,90 @@ export function renderFadeInControl(
         'size': [40, 20],
         'state': false
     });
-    let fadeIn_duration_s: number = 0.010;
+    let fadeIn_duration_s: number = 0.01;
     fadeInControl.on('change', function(useFadeIn: boolean) {
         spectrogramPlaybackManager.setFadeIn(useFadeIn ? fadeIn_duration_s : 0);
     });
 }
 
+interface NoteEventWithChannel extends NoteEvent {
+    channel?: IMidiChannel
+}
+
+export class MultiChannelSpectrogramPlaybackManager extends SpectrogramPlaybackManager {
+    readonly numVoices: number = 16;  // for MIDI playback
+    protected voices_A: Tone.Sampler[];
+    protected voices_B: Tone.Sampler[];
+
+    constructor(locator: SpectrogramLocator) {
+        super(locator);
+
+        this.voices_A = Array(this.numVoices).fill(0).map(() => {
+            return new Tone.Sampler({'C4': new Tone.Buffer(this.buffer_A.get())}).connect(this.crossFade.a);
+        });
+        this.voices_B = Array(this.numVoices).fill(0).map(() => {
+            return new Tone.Sampler({'C4': new Tone.Buffer(this.buffer_B.get())}).connect(this.crossFade.b);
+        });
+    }
+
+    protected onkeydown(data: NoteEventWithChannel) {
+        this.getSamplersByMidiChannel(data.channel).forEach(
+            (voice) => voice.triggerAttack(data.note, undefined, data.velocity)
+        );
+        return this;
+    }
+
+    protected onkeyup(data: NoteEventWithChannel) {
+        this.getSamplersByMidiChannel(data.channel).forEach((voice) => {
+            voice.triggerRelease(data.note);
+        });
+        return this
+    }
+
+    get currentVoices(): Tone.Sampler[] {
+        return this.currentPlayerIsA() ? this.voices_A : this.voices_B;
+    }
+
+    protected getSamplersByMidiChannel(channel: IMidiChannel): Tone.Sampler[] {
+        if ( channel === "all") {
+            return this.currentVoices;
+        } else if ( Array.isArray(channel) ) {
+            return this.currentVoices.filter((value, index) => {
+                channel.contains(index+1)
+            })
+        } else {
+            return [this.currentVoices[channel-1]]
+        }
+    }
+
+    protected updateBuffers(): void {
+        // this.currentVoices.forEach((voice) => voice.set({urls: {
+        //     'C4': this.currentBuffer().get()}})
+        // );
+        this.voices_A.forEach((voice) => {voice.dispose()});
+        this.voices_A.clear();
+        this.voices_A = Array(this.numVoices).fill(0).map(() => {
+            return new Tone.Sampler({'C4': new Tone.Buffer(this.buffer_A.get())}).connect(this.crossFade.a);
+        });
+        this.voices_B.forEach((voice) => {voice.dispose()});
+        this.voices_B.clear();
+        this.voices_B = Array(this.numVoices).fill(0).map(() => {
+            return new Tone.Sampler({'C4': new Tone.Buffer(this.buffer_B.get())}).connect(this.crossFade.b);
+        });
+    }
+
+    async loadAudio(audioURL: string): Promise<void> {
+        await super.loadAudio(audioURL);
+        this.voices_A.forEach((voice) => {voice.add('C4', this.buffer_A.get());});
+        this.voices_B.forEach((voice) => {voice.add('C4', this.buffer_B.get());});
+    }
+
+    setFadeIn(duration_s: number) {
+        super.setFadeIn(duration_s);
+        this.voices_A.forEach((voice) => {voice.attack = duration_s;})
+        this.voices_B.forEach((voice) => {voice.attack = duration_s;})
+    }
+}
 
 export function renderGainControl(
         element: HTMLElement,
