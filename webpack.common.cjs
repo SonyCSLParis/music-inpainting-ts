db4a243a7cf3b5da5c1a513a842a5673cb273793
Théis Bazin <theis.bazin@outlook.com>
2021-02-08 17:10:20 +0100

    :arrow_up: :hammer: Upgrade dependencies
    * Upgrade webpack and fix configuration files
    * Force webpack configuration files to use CommonJS syntax

--- webpack.common.cjs
+++ webpack.common.cjs
@@ -1,4 +1,4 @@
-import webpack from 'webpack'
+const webpack = require('webpack')
 
 module.exports = {
     plugins: [
--- webpack.notono_web_anonymous.cjs
+++ webpack.notono_web_anonymous.cjs
@@ -1,39 +1,46 @@
 // webpack configuration for web, NOTONO-only interface
 // TODO(theis): disable inclusion of sound samples for this mode
 
-import webpack from 'webpack'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import merge from 'webpack-merge'
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { merge, mergeWithCustomize, customizeArray } = require('webpack-merge')
 
-import web from './webpack.web.js'
+const notono_web = require('./webpack.notono_web.cjs')
 
 // DefinePlugin must be overridden by prepending since the inlining occurs
 // in the order of apparition of the multiple instances of the plugin
-let merged_defines = merge.strategy(
-    {plugins: 'prepend'}
-)(web, {
+const merged_defines = mergeWithCustomize({
+    customizeArray: customizeArray({
+        plugins: 'prepend'
+    })
+})(notono_web, {
     mode: 'production',
-
-    devtool: false,
 
     plugins: [
         new webpack.DefinePlugin({
             'SPECTROGRAM_ONLY': true,
+            'ENABLE_ANONYMOUS_MODE': true
         }),
     ]
 });
 
-export default merge(
+module.exports = merge(
     merged_defines,
     {
+        output: {
+            path: path.join(__dirname, 'web/dist_anonymous'),
+            filename: 'index.bundle.js'
+        },
+
         plugins: [
             new HtmlWebpackPlugin({
                 meta: {
                     // Fixes 300ms delay on touch + reduce size on mobile for better display
                     'viewport': "width=device-width, initial-scale=0.5, maximum-scale=1.0, user-scalable=no, target-densityDpi=medium-dpi"
                 },
-                title: 'NOTONO',
-                favicon: 'src/common/images/favicon.ico'
+                title: 'NSynth Vector-Quantized Inpainting',
+                favicon: 'src/common/images/favicon_spectrogram.ico'
         })]
     }
 );
