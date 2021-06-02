3313bc0a563c318dcaf18d80e667830d2de658a2
Théis Bazin <theis.bazin@outlook.com>
2021-06-02 15:22:52 +0200

    :sparkles: Allow to easily open multiple windows

--- src/main/main.ts
+++ src/main/main.ts
@@ -31,11 +31,7 @@ app.on('window-all-closed', function () {
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (!WindowManager.existsWindow()) {
-    WindowManager.createWindow()
-  }
+  WindowManager.createWindow()
 })
 
 ipcMain.on('disconnect', () => LinkServer.killLink())
