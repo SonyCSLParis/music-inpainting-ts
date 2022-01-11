93de402e0d3892b55ebc4a444493992c81c7de1b
Théis Bazin <theis.bazin@outlook.com>
2022-01-11 16:54:59 +0100

    :art: Refactor locator->inpainter, cleanup spinner

--- src/renderer/randomizer.ts
+++ src/renderer/randomizer.ts
@@ -1,26 +1,26 @@
 import Tone from 'tone'
 
 import { PlaybackManager } from './playback'
-import { Locator } from './locator'
+import { Inpainter } from './inpainter'
 
 class LowFrequencyRandomizer<
   PlaybackManagerT extends PlaybackManager
 > extends Tone.Loop {
   protected container: HTMLElement
-  readonly locator: Locator<PlaybackManagerT, unknown>
+  readonly inpainter: Inpainter<PlaybackManagerT, unknown>
 
   // numCells: number = 1;
   constructor(
-    locator: Locator<PlaybackManagerT, unknown>,
+    inpainter: Inpainter<PlaybackManagerT, unknown>,
     callback: (time: number, numCells?: number) => void,
     interval: Tone.Unit.Time = '4n'
   ) {
     super((time) => {}, interval)
-    this.locator = locator
+    this.inpainter = inpainter
   }
 
   callback: (time: number) => void = (time: number) => {
-    this.locator
+    this.inpainter
   }
 
   protected render(container: HTMLElement, registerAsAdvancedControl = false) {
