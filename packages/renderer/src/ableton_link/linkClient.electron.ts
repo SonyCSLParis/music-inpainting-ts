f73a04694c4a17dfbd93d96df6e34a25850fae42
Théis Bazin <theis.bazin@outlook.com>
2022-05-09 21:34:27 +0200

    :construction: Initial LinkClientElectron rewrite

--- packages/renderer/src/ableton_link/linkClient.electron.ts
+++ packages/renderer/src/ableton_link/linkClient.electron.ts
@@ -5,7 +5,7 @@ import default_config from '../../../common/default_config.json'
 import { BPMControl } from '../numberControl'
 
 const VITE_COMPILE_ELECTRON = import.meta.env.VITE_COMPILE_ELECTRON != undefined
-const ipcRenderer = VITE_COMPILE_ELECTRON ? window.ipcRenderer : null
+const ipcRenderer = window.ipcRendererInterface
 
 const link_channel_prefix: string = default_config['link_channel_prefix']
 
@@ -14,7 +14,7 @@ export class LinkClientElectron extends AbletonLinkClient {
   protected _windowID?: number
   protected async windowID(): Promise<number> {
     if (this._windowID == null) {
-      this._windowID = <number>await ipcRenderer?.invoke('get-window-id')
+      this._windowID = <number>await ipcRenderer.getWindowId()
     }
     return this._windowID
   }
@@ -28,38 +28,40 @@ export class LinkClientElectron extends AbletonLinkClient {
   }
 
   // Schedule a LINK dependent callback
-  async onServerMessage(
-    message: string,
-    callback: IpcRendererCallback
-  ): Promise<this> {
-    ipcRenderer?.on(await this.prefixMessage(message), callback.bind(this))
-    return this
-  }
+  // async onServerMessage(
+  //   message: string,
+  //   callback: IpcRendererCallback
+  // ): Promise<this> {
+  //   window.abletonLinkApi.on(await this.prefixMessage(message), callback.bind(this))
+  //   return this
+  // }
   // Schedule a LINK dependent callback once
-  async onServerMessageOnce(
-    message: string,
-    callback: IpcRendererCallback
-  ): Promise<this> {
-    ipcRenderer?.once(await this.prefixMessage(message), callback.bind(this))
-    return this
-  }
+  // async onServerMessageOnce(
+  //   message: string,
+  //   callback: IpcRendererCallback
+  // ): Promise<this> {
+  //   ipcRenderer.once(await this.prefixMessage(message), callback.bind(this))
+  //   return this
+  // }
   // Send values to the Ableton Link server
-  async sendToServer(message: string, ...args: any[]): Promise<void> {
-    ipcRenderer?.send(await this.prefixMessage(message), ...args)
-  }
-  async removeServerListener(
-    message: string,
-    callback: IpcRendererCallback
-  ): Promise<void> {
-    ipcRenderer?.removeListener(await this.prefixMessage(message), callback)
-  }
-  async removeAllServerListeners(message: string): Promise<void> {
-    ipcRenderer?.removeAllListeners(await this.prefixMessage(message))
-  }
+  // async sendToServer(message: string, ...args: any[]): Promise<void> {
+  //   ipcRenderer.send(await this.prefixMessage(message), ...args)
+  // }
+  // async removeServerListener(
+  //   message: string,
+  //   callback: IpcRendererCallback
+  // ): Promise<void> {
+  //   ipcRenderer.removeListener(await this.prefixMessage(message), callback)
+  // }
+  // async removeAllServerListeners(message: string): Promise<void> {
+  //   ipcRenderer.removeAllListeners(await this.prefixMessage(message))
+  // }
 
   async getPhaseAsync(): Promise<number> {
-    return <Promise<number>>(
-      ipcRenderer?.invoke(await this.prefixMessage('get-phase'))
-    )
+    throw Error("Not implemented")
+    //   return <Promise<number>>(
+    //     this.windowID
+    //     // window.abletonLinkApi.ipcRenderer.invoke(await this.prefixMessage('get-phase'))
+    //   )
   }
 }
