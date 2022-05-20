509d3cadd1c202504be62bb048c25ed432d71546
Théis Bazin <theis.bazin@outlook.com>
2022-05-20 18:14:39 +0200

    :mag: :bug: Re-add open-graph data and favicon

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -3,30 +3,100 @@
 import { chrome } from '../../.electron-vendors.cache.json'
 import path, { join } from 'path'
 import { builtinModules } from 'module'
+import { HtmlTagDescriptor, IndexHtmlTransformHook, UserConfig } from 'vite'
 
 const PACKAGE_ROOT = __dirname
 
-console.log(PACKAGE_ROOT)
-console.log(
-  path.resolve(PACKAGE_ROOT, '../../node_modules/webmidi/dist/esm/') + '/'
-)
-
+const VITE_DEPLOYMENT_URL =
+  process.env.VITE_DEPLOYMENT_URL != undefined
+    ? new URL(process.env.VITE_DEPLOYMENT_URL).href
+    : undefined
 const VITE_COMPILE_WEB = process.env.VITE_COMPILE_WEB != undefined
 const VITE_APP_TITLE =
   process.env.VITE_APP_TITLE != undefined
     ? process.env.VITE_APP_TITLE
     : 'VITE_APP'
 
+function makeOpenGraphData(): HtmlTagDescriptor[] {
+  let tags: Map<string, string>
+  if (!VITE_COMPILE_WEB) {
+    return []
+  }
+  if (VITE_APP_TITLE.toLowerCase() == 'notono') {
+    tags = new Map([
+      [
+        'og:title',
+        'NOTONO: AI-assisted visual transformation of musical sounds',
+      ],
+      [
+        'og:description',
+        'Let your visual thinking draw new sounds. An AI-based interface for inpainting of musical sounds, developed at Sony CSL Paris.',
+      ],
+      ['og:type', 'website'],
+      ['og:url', VITE_DEPLOYMENT_URL || ''],
+      [
+        'og:image',
+        new URL('notono-preview-20220511-1200_630.jpg', VITE_DEPLOYMENT_URL)
+          .href,
+      ],
+      ['og:image:width', '1200'],
+      ['og:image:height', '630'],
+      ['og:image:alt', 'An screenshot of the NOTONO interface is shown.'],
+    ])
+  } else if (VITE_APP_TITLE.toLowerCase() == 'nonoto') {
+    tags = new Map([
+      [
+        'og:title',
+        'NONOTO+DeepBach: AI-assisted interactive generation of chorale music by inpainting',
+      ],
+      [
+        'og:description',
+        'Easily create polyphonic music with just the tip of your finger, all in your browser. Developed at Sony CSL Paris.',
+      ],
+      ['og:type', 'website'],
+      ['og:url', VITE_DEPLOYMENT_URL || ''],
+      [
+        'og:image',
+        new URL('nonoto-preview-20220511-1200_630.jpg', VITE_DEPLOYMENT_URL)
+          .href,
+      ],
+      ['og:image:width', '1200'],
+      ['og:image:height', '630'],
+      ['og:image:alt', 'An screenshot of the NONOTO interface is shown.'],
+    ])
+  }
+  const toTag = ([attrType, content]: [string, string]): HtmlTagDescriptor => {
+    const attrs = {}
+    attrs['property'] = attrType
+    attrs['content'] = content
+    return { tag: 'meta', attrs: attrs }
+  }
+  return Array.from(tags.entries()).map(toTag)
+}
+
+const indexHtmlTransformHook: IndexHtmlTransformHook = (html, ctx) => {
+  html = html.replace(
+    /<title>(.*?)<\/title>/,
+    `<title>${VITE_APP_TITLE}</title>`
+  )
+  const faviconTag = {
+    tag: 'link',
+    attrs: {
+      rel: 'shortcut icon',
+      href: new URL('favicon.ico', VITE_DEPLOYMENT_URL).href,
+    },
+  }
+  const tags: HtmlTagDescriptor[] = [
+    faviconTag,
+    ...makeOpenGraphData(),
+  ]
+  return { html: html, tags: tags }
+}
+
 const htmlPlugin = () => {
   return {
     name: 'html-transform',
-    transformIndexHtml: (html: string): string => {
-      const updateTitle = html.replace(
-        /<title>(.*?)<\/title>/,
-        `<title>${VITE_APP_TITLE}</title>`
-      )
-      return updateTitle
-    },
+    transformIndexHtml: indexHtmlTransformHook,
   }
 }
 
@@ -34,7 +104,7 @@ const htmlPlugin = () => {
  * @type {import('vite').UserConfig}
  * @see https://vitejs.dev/config/
  */
-const config = {
+const config: UserConfig = {
   mode: process.env.MODE,
   root: PACKAGE_ROOT,
   resolve: {
@@ -54,6 +124,7 @@ const config = {
       strict: true,
     },
   },
+  publicDir: './public',
   build: {
     sourcemap: true,
     target: `chrome${chrome}`,
