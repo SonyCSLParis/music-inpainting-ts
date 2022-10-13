6a8fccea3b3be08aef446be0fc8926f046a76a74
Théis Bazin <theis.bazin@outlook.com>
2022-10-13 04:25:59 +0200

    :wrench: Add create-invalidation to script

--- scripts/sync_to_s3.sh
+++ scripts/sync_to_s3.sh
@@ -17,3 +17,6 @@ aws s3 cp $BUILD_DIR/index.html s3://$S3_BUCKET_NAME/index.html \
   --metadata-directive REPLACE \
   --cache-control max-age=0,no-cache,no-store,must-revalidate \
   --content-type text/html
+
+echo "Invalidating Cloudfront distribution"
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths '/*'
